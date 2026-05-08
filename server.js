a6bb8bedea216e26f03a3a76a93c5346966ce6dc
ventasmedinsur19-tech <ventasmedinsur19@gmail.com>
2026-05-08 16:23:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,41 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+let gameState = {
+    letra: "",
+    jugadores: {},
+    juegoActivo: false
+};
+
+const letras = "ABCDEFGHIJKLMNOPRSTUV";
+
 io.on('connection', (socket) => {
-  console.log('Alguien se conectó');
+    // Al conectarse, enviamos el estado actual al nuevo jugador
+    socket.emit('init', gameState);
 
-  socket.on('nuevo-jugador', (nombre) => {
-    console.log(nombre + ' se unió al juego');
-  });
+    socket.on('unirse', (nombre) => {
+        gameState.jugadores[socket.id] = { nombre, listo: true };
+        io.emit('actualizar-jugadores', Object.values(gameState.jugadores));
+    });
 
-  socket.on('presionó-basta', () => {
-    // Esto le avisa a TODOS los que estén conectados
-    io.emit('bloquear-todo');
-  });
+    socket.on('empezar-ronda', () => {
+        if (!gameState.juegoActivo) {
+            gameState.juegoActivo = true;
+            gameState.letra = letras[Math.floor(Math.random() * letras.length)];
+            io.emit('ronda-iniciada', gameState.letra);
+        }
+    });
+
+    socket.on('basta', () => {
+        gameState.juegoActivo = false;
+        io.emit('bloquear-y-recaudar', { ganador: gameState.jugadores[socket.id]?.nombre });
+    });
+
+    socket.on('disconnect', () => {
+        delete gameState.jugadores[socket.id];
+        io.emit('actualizar-jugadores', Object.values(gameState.jugadores));
+    });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log('Servidor de TuttiFrutti listo');
-});
+http.listen(PORT, () => console.log(`Puerto: ${PORT}`));
